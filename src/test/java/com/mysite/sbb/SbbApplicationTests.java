15989361f75aaa3c568a85f30ad8b9d77aef3f70
ujinkim <ujkim25@gmail.com>
2025-01-22 15:29:43 +0900

    Answer entity data inquiry

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -10,10 +10,11 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class SbbApplicationTests {
-	@Autowired // 스프링의 의존성 주입(DI) : 스프링이 객체를 대신 생성하여 주입하는 기법
+	@Autowired //스프링의 의존성 주입(DI) : 스프링이 객체를 대신 생성하여 주입하는 기법
 	/*객체를 주입하는 방식에는 @Autowired 애너테이션을 사용하는 것 외에 Setter 메서드 또는 생성자를 사용하는 방식이 있다.
 	 개발 시 @Autowired보다는 생성자를 통한 객체 주입 방식을 권장한다.*/
 	QuestionRepository questionRepository;
@@ -21,11 +22,15 @@ class SbbApplicationTests {
 	@Autowired
 	AnswerRepository answerRepository;
 	
+	@Transactional //메서드가 종료될 때까지 DB 세션이 유지된다.
 	@Test
 	void testJpa() {
-		Optional<Answer> oa = this.answerRepository.findById(1);
-		assertTrue(oa.isPresent());
-		Answer answer = oa.get();
-		assertEquals(2, answer.getQuestion().getId());
+		List<Question> questions = questionRepository.findAll(); //테스트 코드에서는 여기에서 DB 세션이 끊어진다.
+		Question question = questions.get(0);
+		
+		List<Answer> answers = question.getAnswerList();
+		
+		Answer answer = answers.get(0);
+		assertEquals(1, answer.getId());
 	}
 }
