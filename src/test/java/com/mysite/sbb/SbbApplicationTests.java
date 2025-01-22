1989f8a07727b0c654072a9ea4edbbb6402e5774
ujinkim <ujkim25@gmail.com>
2025-01-22 09:46:50 +0900

    findBySubjectLike()

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -18,7 +18,8 @@ class SbbApplicationTests {
 	
 	@Test
 	void testJpa() {
-		Question question = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-		assertEquals(1,question.getId());
+		List<Question> questions = this.questionRepository.findBySubjectLike("%sbb%");
+		Question question = questions.get(0);
+		assertEquals(1, question.getId());
 	}
 }
