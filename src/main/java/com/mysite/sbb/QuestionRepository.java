ddad9aceca276fd95dd95ce0062643238aa2eacf
ujinkim <ujkim25@gmail.com>
2025-01-22 09:35:51 +0900

    findBySubjectAndContent()

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 	// Question 엔티티로 리포지터리를 생성한다는 의미. Question 엔티티의 기본키가 Integer임을 이와 같이 추가로 지정
 	Question findBySubject(String subject);
+	Question findBySubjectAndContent(String s1, String s2);
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -18,9 +18,7 @@ class SbbApplicationTests {
 	
 	@Test
 	void testJpa() {
-		Question question = questionRepository.findBySubject("sbb가 무엇인가요?");
-		/*JPA에 리포지터리의 메서드명을 분석하여 쿼리를 만들고 실행하는 기능이 있기 때문에 가능하다.
-		 즉, 여러분은 findBy + 엔티티의 속성명(예를 들어 findBySubject)과 같은 리포지터리의 메서드를 작성하면 입력한 속성의 값으로 데이터를 조회할 수 있다!*/
-		assertEquals(1, question.getId());
+		Question question = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		assertEquals(1,question.getId());
 	}
 }
