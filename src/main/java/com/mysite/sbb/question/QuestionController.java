78a90fecc79123db0f2a9e23c71de55683d84bff
ujinkim <ujkim25@gmail.com>
2025-01-24 17:53:00 +0900

    Form Class Input Validation

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -9,8 +9,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,13 +48,19 @@ public class QuestionController {
 	}
 	
 	@GetMapping("/create")
-	String questionCreate() {
+	String questionCreate(QuestionForm questionForm) {//question_create.html은 [질문 등록하기] 버튼을 통해 GET 방식으로 URL이 요청되더라도 th:object에 의해 QuestionForm 객체가 필요하다.
 		return "question_create";
 	}
 	
 	@PostMapping("/create")
-	String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
-		this.questionService.create(subject, content);
+	String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+		//@Valid 애너테이션을 적용하면 QuestionForm의 @NotEmpty, @Size 등으로 설정한 검증 기능이 동작한다.
+		//BindingResult 매개변수는 @Valid 애너테이션으로 검증이 수행된 결과를 의미하는 객체이다.
+		//subject, content 항목을 지닌 폼이 전송되면 QuestionForm의 subject, content 속성이 자동으로 바인딩된다. 이렇게 이름이 동일하면 함께 연결되어 묶이는 것이 바로 폼의 바인딩 기능이다.
+		if (bindingResult.hasErrors()) {
+			return "question_create";
+		}
+		this.questionService.create(questionForm.getSubject(), questionForm.getContent());
 		return "redirect:/question/list"; //[저장하기] 버튼을 클릭해 질문이 저장되면 질문 목록 페이지로 이동
 	}
 }
